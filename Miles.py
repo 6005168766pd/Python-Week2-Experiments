2c80255a31dd67fde50df3edfed9c95af4ad5f25
kakaunknown2024-debug <kakaunknown2024@gmail.com>
2026-03-27 00:42:38 +0530

    Update Miles.py

--- Miles.py
+++ Miles.py
@@ -1,3 +1,5 @@
+# Program to convert distance in kms to miles
 km=int(input("Enter the distance in kms:"))
 miles=0.621371*km
 print("The distance in Miles is:",miles)
+print("Program Execution Completed")
