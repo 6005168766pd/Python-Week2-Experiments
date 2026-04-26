cc69f9b456584ab35ab2e6fdd26d6ba535624d82
Prem Dogra <officialprem707@gmail.com>
2026-04-26 20:49:00 +0530

    Update Quadratic.py

--- Quadratic.py
+++ Quadratic.py
@@ -2,8 +2,8 @@ import cmath
 a=int(input("Enter the co_effiecient of x^2(a):"))
 b=int(input("Enter the co_effiecient of x(b):"))
 c=int(input("Enter the constant value(c):"))
-d=b**2-4*(a)*(c)
-root1=(-b+cmath.sqrt(d))/2*a;
-root2=(-b-cmath.sqrt(d))/2*a;
+D=b**2-4*(a)*(c)
+root1=(-b+cmath.sqrt(D))/2*a;
+root2=(-b-cmath.sqrt(D))/2*a;
 print("Roots of quadratic equation are:",root1,root2)
 
